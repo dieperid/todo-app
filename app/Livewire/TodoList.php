9ecb3668bf7744bb6d0cb3de8f105a3c52183da1
dieperid <david.dieperink@ik.me>
2024-07-04 14:14:25 +0200

    includes folder for splitting CRUD, added create method

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    /**
+     * Name of the Todo
+     */
+    #[Rule('required|min:3|max:50')]
+    public $name;
+
+    public $search;
+
+    public function create()
+    {
+        $validatedItem = $this->validateOnly('name');
+
+        Todo::create($validatedItem);
+
+        $this->reset('name');
+
+        session()->flash('success', 'Todo has been created');
+    }
+
     public function render()
     {
         return view('livewire.todo-list');
